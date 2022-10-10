a407730e7b197e566ca18a664a37074b272aee1e
habubey <habubey@outlook.com>
2022-10-10 16:04:35 +0300

    add auth pages: access and error

---
+++ pages/auth/error/index.js
@@ -0,0 +1,37 @@
+import getConfig from 'next/config';
+import React, { useContext } from 'react';
+import { LayoutContext } from '../../../layout/context/layoutcontext';
+
+const Error = () => {
+    const { config } = useContext(LayoutContext);
+    const contextPath = getConfig().publicRuntimeConfig.contextPath;
+    return (
+        <div className="surface-0 flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+            <div className="grid justify-content-center p-2 lg:p-0" style={{ minWidth: '80%' }}>
+                <div className="col-12 mt-5 xl:mt-0 text-center">
+                    <img src={`${contextPath}/demo/images/error/logo-error.svg`} alt="Sakai logo" className="mb-5" style={{ width: '81px', height: '60px' }} />
+                </div>
+                <div className="col-12 xl:col-6" style={{ borderRadius: '56px', padding: '0.3rem', background: 'linear-gradient(180deg, rgba(233, 30, 99, 0.4) 10%, rgba(33, 150, 243, 0) 30%)' }}>
+                    <div className="h-full w-full m-0 py-7 px-4" style={{ borderRadius: '53px', background: 'linear-gradient(180deg, var(--surface-50) 38.9%, var(--surface-0))' }}>
+                        <div className="grid flex flex-column align-items-center">
+                            <div className="flex justify-content-center align-items-center bg-pink-500 border-circle" style={{ height: '3.2rem', width: '3.2rem' }}>
+                                <i className="pi pi-fw pi-exclamation-circle text-2xl text-white"></i>
+                            </div>
+                            <h1 className="text-900 font-bold text-5xl mb-2">Error Occured</h1>
+                            <span className="text-600">Requested resource is not available.</span>
+                            <img src={`${contextPath}/demo/images/error/asset-error.svg`} alt="Error" className="mt-5" width="80%" />
+                            <div className="col-12 mt-5 text-center">
+                                <i className="pi pi-fw pi-arrow-left text-blue-500 mr-2" style={{ verticalAlign: 'center' }}></i>
+                                <a href="#" className="text-blue-500">
+                                    Go to Dashboard
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
