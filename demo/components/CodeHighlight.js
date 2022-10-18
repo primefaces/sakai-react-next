e5c7b2b2ebe44e3d6fd2f4f93878e546874a0733
Cagatay Civici <cagatay.civici@gmail.com>
2022-10-18 16:03:31 +0300

    Add prism

--- demo/components/CodeHighlight.js
+++ demo/components/CodeHighlight.js
@@ -1,25 +1,18 @@
-import { useMountEffect } from 'primereact/hooks';
-import React, { useRef } from 'react';
-import 'prismjs/themes/prism-coy.css';
+import React, { useEffect, useRef } from 'react';
+
 export function CodeHighlight(props) {
     const codeElement = useRef();
     const languageClassName = `language-${props.lang || 'jsx'}`;
 
-    useMountEffect(() => {
-        import('prismjs').then((module) => {
-            if (module) {
-                module.highlightElement(codeElement.current);
-            }
-        });
+    useEffect(() => {
+        window.Prism.highlightElement(codeElement.current);
     }, []);
 
     return (
-        <div>
-            <pre style={props.style}>
-                <code ref={codeElement} className={languageClassName}>
-                    {props.children}&nbsp;
-                </code>
-            </pre>
-        </div>
+        <pre style={props.style}>
+            <code ref={codeElement} className={languageClassName}>
+                {props.children}&nbsp;
+            </code>
+        </pre>
     );
 }
