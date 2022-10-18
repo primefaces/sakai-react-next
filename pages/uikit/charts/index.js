d4acc56bdc111208578f4cf17b8d0880b90e645d
Cagatay Civici <cagatay.civici@gmail.com>
2022-10-18 15:37:06 +0300

    Fixed charts layout

--- pages/uikit/charts/index.js
+++ pages/uikit/charts/index.js
@@ -224,36 +224,40 @@ const ChartDemo = () => {
 
     return (
         <div className="grid p-fluid">
-            <div className="col-12 lg:col-6">
+            <div className="col-12 xl:col-6">
                 <div className="card">
                     <h5>Linear Chart</h5>
                     <Chart type="line" data={data.lineData} options={options.lineOptions}></Chart>
                 </div>
-
-                <div className="card flex flex-column align-items-center">
-                    <h5 className="text-left w-full">Pie Chart</h5>
-                    <Chart type="pie" data={data.pieData} options={options.pieOptions} style={{ width: '50%' }}></Chart>
-                </div>
-
-                <div className="card flex flex-column align-items-center">
-                    <h5 className="text-left w-full">Polar Area Chart</h5>
-                    <Chart type="polarArea" data={data.polarData} options={options.polarOptions} style={{ width: '50%' }}></Chart>
-                </div>
             </div>
-            <div className="col-12 lg:col-6">
+            <div className="col-12 xl:col-6">
                 <div className="card">
                     <h5>Bar Chart</h5>
                     <Chart type="bar" data={data.barData} options={options.barOptions}></Chart>
                 </div>
-
+            </div>
+            <div className="col-12 xl:col-6">
+                <div className="card flex flex-column align-items-center">
+                    <h5 className="text-left w-full">Pie Chart</h5>
+                    <Chart type="pie" data={data.pieData} options={options.pieOptions}></Chart>
+                </div>
+            </div>
+            <div className="col-12 xl:col-6">
                 <div className="card flex flex-column align-items-center">
                     <h5 className="text-left w-full">Doughnut Chart</h5>
-                    <Chart type="doughnut" data={data.pieData} options={options.pieOptions} style={{ width: '50%' }}></Chart>
+                    <Chart type="doughnut" data={data.pieData} options={options.pieOptions}></Chart>
                 </div>
-
+            </div>
+            <div className="col-12 xl:col-6">
+                <div className="card flex flex-column align-items-center">
+                    <h5 className="text-left w-full">Polar Area Chart</h5>
+                    <Chart type="polarArea" data={data.polarData} options={options.polarOptions}></Chart>
+                </div>
+            </div>
+            <div className="col-12 xl:col-6">
                 <div className="card flex flex-column align-items-center">
                     <h5 className="text-left w-full">Radar Chart</h5>
-                    <Chart type="radar" data={data.radarData} options={options.radarOptions} style={{ width: '50%' }}></Chart>
+                    <Chart type="radar" data={data.radarData} options={options.radarOptions}></Chart>
                 </div>
             </div>
         </div>
