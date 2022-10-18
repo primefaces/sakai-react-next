ff98dd751db856986c5d5f7a3d002adea508e56d
Cagatay Civici <cagatay.civici@gmail.com>
2022-10-18 14:34:12 +0300

    Fixed order

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { LayoutProvider } from '../layout/context/layoutcontext';
 import Layout from '../layout/layout';
-import '../styles/layout/layout.scss';
 import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
+import '../styles/layout/layout.scss';
 import '../styles/demo/Demos.scss';
 
 export default function MyApp({ Component, pageProps }) {
