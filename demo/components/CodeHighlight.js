1dd23a3a55c49f172ccde319c1a9a982d3d7ad43
Cagatay Civici <cagatay.civici@gmail.com>
2022-10-19 11:33:02 +0300

    Better ultra wide support

--- demo/components/CodeHighlight.js
+++ demo/components/CodeHighlight.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function CodeHighlight(props) {
     return (
-        <pre {...props} className="border-round surface-ground text-700 p-5">
+        <pre {...props} className="border-round surface-ground text-700 p-5 overflow-auto">
             <code className="-mt-4 p-0 line-height-3 block">{props.children}</code>
         </pre>
     );
